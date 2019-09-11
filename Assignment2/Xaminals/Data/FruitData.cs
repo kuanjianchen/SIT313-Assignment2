edf8f15773aef9d23e1ff3f3e20066490e2446bf
ZiChao Kuang <kuanjianchen@gmail.com>
2019-09-11 18:25:38 +1000

    week8 update(3)
    items data

---
+++ Assignment2/Xaminals/Data/FruitData.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Xaminals.Models;
+
+namespace Xaminals.Data
+{
+    public static class FruitData
+    {
+        public static IList<Grocery> Fruits { get; private set; }
+
+        static FruitData()
+        {
+            Fruits = new List<Grocery>();
+
+            Fruits.Add(new Grocery
+            {
+                Name = "Apple",
+                Location = "Africa & Asia",
+                Details = " .fhfjfjfjkjfjfj",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/3/32/Dark_apple.png"
+            });
+
+            Fruits.Add(new Grocery
+            {
+                Name = "Mango",
+                Location = "Central & South America",
+                Details = ".vhhhhh",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Mango_and_cross_section_edit.jpg/1600px-Mango_and_cross_section_edit.jpg"
+            });
+
+            Fruits.Add(new Grocery
+            {
+                Name = "Orange",
+                Location = "Central and East Africa",
+                Details = "",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Orange-Fruit-Pieces.jpg/1600px-Orange-Fruit-Pieces.jpg"
+            });
+
+            Fruits.Add(new Grocery
+            {
+                Name = "Pine Apple",
+                Location = "Central & South America",
+                Details = "",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/e1/Ananas.jpg"
+            });
+
+            Fruits.Add(new Grocery
+            {
+                Name = "Grapes",
+                Location = "Brazil",
+                Details = "",
+                 ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Wine_grapes03.jpg/794px-Wine_grapes03.jpg"
+            });
+
+            Fruits.Add(new Grocery
+            {
+                Name = "Banana",
+                Location = "South America",
+                Details = ".",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/1/1c/Bananas_white_background.jpg"
+            });
+
+           
+
+           
+        }
+    }
+}
---
+++ Assignment2/Xaminals/Data/VegetableData.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Xaminals.Models;
+
+namespace Xaminals.Data
+{
+    public static class VegetableData
+    {
+        public static IList<Grocery> Vegetables { get; private set; }
+
+        static VegetableData()
+        {
+            Vegetables = new List<Grocery>
+            {
+                new Grocery
+                {
+                    Name = "Onions",
+                    Location = "Africa & Asia",
+                    Details = "red and brown onions, with and without skin, whole and sliced and in rings",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Mixed_onions.jpg/1280px-Mixed_onions.jpg"
+                },
+
+                new Grocery
+                {
+                    Name = "Potatoes",
+                    Location = "Central & South America",
+                    Details = ".",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/1920px-Patates.jpg"
+                },
+
+                new Grocery
+                {
+                    Name = "Garlic",
+                    Location = "Central and East Africa",
+                    Details = "",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Allium_sativum._Restra_de_allos_de_Oroso-_Galiza.jpg/1920px-Allium_sativum._Restra_de_allos_de_Oroso-_Galiza.jpg"
+                },
+
+                new Grocery
+                {
+                    Name = "Cabbage",
+                    Location = "Central & South America",
+                    Details = "",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Cabbages_Green_and_Purple_2120px.jpg/1024px-Cabbages_Green_and_Purple_2120px.jpg"
+                },
+
+                new Grocery
+                {
+                    Name = "Sweet Corn",
+                    Location = "Brazil",
+                    Details = "",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/7/79/VegCorn.jpg"
+                },
+
+                new Grocery
+                {
+                    Name = "Tomatoes",
+                    Location = "South America",
+                    Details = ".",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/5/50/%E0%B0%9F%E0%B0%AE%E0%B0%BE%E0%B0%9F%E0%B0%BE%E0%B0%B2%E0%B1%81_%282%29.jpg"
+                }
+            };
+
+
+
+
+        }
+    }
+}
