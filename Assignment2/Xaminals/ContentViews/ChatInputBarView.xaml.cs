612d4242b2414d009ec6cb47ea3b86bdbc251d85
ZiChao Kuang <kuanjianchen@gmail.com>
2019-09-16 14:49:43 +1000

    Week9 update
    weekly update

---
+++ Assignment2/Xaminals/ContentViews/ChatInputBarView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xaminals.ViewModels;
+
+namespace Xaminals.ContentViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatInputBarView : Xamarin.Forms.ContentView
+    {
+        public ChatInputBarView()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                this.SetBinding(HeightRequestProperty, new Binding("Height", BindingMode.OneWay, null, null, null, chatTextInput));
+            }
+        }
+
+        public void Handle_Completed(object sender, EventArgs e)
+        {
+            (this.Parent.Parent.BindingContext as ChatPageViewModel).OnSendCommand.Execute(null);
+            chatTextInput.Focus();
+        }
+
+        public void UnFocusEntry()
+        {
+            chatTextInput?.Unfocus();
+        }
+    }
+}
